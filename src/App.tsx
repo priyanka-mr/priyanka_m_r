ad39c15a453b34280736ed8132ed8bbbe4dd361a
Priyanka M R <priyankamr@Priyankas-MacBook-Air.local>
2024-02-25 00:58:48 +0800

    profile photo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import myPhoto from './assets/my_photo.jpg';
 
 function App() {
   return (
     <div className="App">
       <p>Priyanka's Website</p>
+      <img src={myPhoto} alt={'priyanka'} />
     </div>
   );
 }
